957cc2a4223854c644eed89a5edc2cfe85db695c
alex-grigits <ts.alex85@gmail.com>
2017-09-27 01:15:49 +0300

    added testimonials and news sections

--- src/blocks/team/slide.js
+++ src/blocks/team/slide.js
@@ -1,26 +1,36 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex, "team-slide", "dots");
+showSlides(slideIndex, "card", "indicators");
+showSlides(slideIndex, "news-slide", "news-slide-indicators");
 
 function plusSlides(n) {
 	showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-	showSlides(slideIndex = n);
+	showSlides(slideIndex = n, "team-slide", "dots");
 }
 
-function showSlides(n) {
+function currentSlide2(n) {
+	showSlides(slideIndex = n, "card", "indicators");
+}
+
+function currentSlide3(n) {
+	showSlides(slideIndex = n, "news-slide", "news-slide-indicators");
+}
+
+function showSlides(n, slidesClass, indicators) {
 	var i;
-	var slides = document.getElementsByClassName("team-slideshow");
-	var dots = document.getElementsByClassName("dots__dot");
+	var slides = document.getElementsByClassName(slidesClass);
+	var dots = document.getElementsByClassName(indicators + "__dot");
 	if (n > slides.length) {slideIndex = 1}
 	if (n < 1) {slideIndex = slides.length}
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
 	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" dots__dot--active", "");
+		dots[i].className = dots[i].className.replace(" " + indicators + "__dot--active", "");
 	}
-	slides[slideIndex-1].style.display = "flex";
-	dots[slideIndex-1].className += " dots__dot--active";
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " " + indicators + "__dot--active";
 }
